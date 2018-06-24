6a2060fc0af61042a726c6ebc30b5afe38bfbe96
Vinayak02 <31791599+Vinayak02@users.noreply.github.com>
2018-06-24 15:51:27 +0530

    added factorial.cpp

---
+++ code/dynamic_programming/src/factorial/factorial.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+int
+factorial(int n)
+{
+	if (n == 0 || n == 1)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
+
+int
+main()
+{
+	int n;
+	printf("Enter a Whole Number: ");
+	scanf("%d", &n);
+	printf("%d! = %d\n", n, factorial(n));
+
+	return (0);
+}
